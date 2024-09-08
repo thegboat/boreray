9f739e91a612260155ff5ad291108e9c7c66d6e5
Grady Griffin <gradygriffin@gmail.com>
2024-09-08 04:36:06 -0400

    first commit

---
+++ lib/boreray.ex
@@ -0,0 +1,18 @@
+defmodule Boreray do
+  @moduledoc """
+  Documentation for `Boreray`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Boreray.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/boreray_test.exs
@@ -0,0 +1,8 @@
+defmodule BorerayTest do
+  use ExUnit.Case
+  doctest Boreray
+
+  test "greets the world" do
+    assert Boreray.hello() == :world
+  end
+end
